8ea9a25908181a355b97d6385726a64832cbae3d
Adam Feher <fhradm@gmail.com>
2018-09-07 16:32:43 +0200

    Some tests in main had no time for unit test ;<

--- src/main/java/com/adamfeher/superchargebankingapp/Main.java
+++ src/main/java/com/adamfeher/superchargebankingapp/Main.java
@@ -1,7 +1,32 @@
 package com.adamfeher.superchargebankingapp;
 
+import com.adamfeher.superchargebankingapp.model.Account;
+import com.adamfeher.superchargebankingapp.model.User;
+import com.adamfeher.superchargebankingapp.service.AccountService;
+import com.adamfeher.superchargebankingapp.service.TransactionService;
+
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
+        User john = new User("John", "john@example.com", "123");
+        User andy = new User("Andy", "andy@example.com", "1234");
+        Account accountOfJohn = new Account(john);
+        Account accountOfAndy = new Account(andy);
 
+        AccountService accountService = new AccountService();
+        TransactionService transactionService = new TransactionService(accountService);
+
+        accountService.deposit(accountOfJohn, BigDecimal.valueOf(10000));
+        accountService.deposit(accountOfAndy, BigDecimal.valueOf(20000));
+        System.out.println(accountOfAndy.getBalance());
+        System.out.println(accountOfJohn.getBalance());
+        accountService.withdrawal(accountOfJohn, BigDecimal.valueOf(1000));
+        accountService.withdrawal(accountOfAndy, BigDecimal.valueOf(5000));
+        System.out.println(accountOfAndy.getBalance());
+        System.out.println(accountOfJohn.getBalance());
+        transactionService.transfer(accountOfJohn, accountOfAndy, BigDecimal.valueOf(2000));
+        System.out.println(accountOfAndy.getBalance());
+        System.out.println(accountOfJohn.getBalance());
     }
 }

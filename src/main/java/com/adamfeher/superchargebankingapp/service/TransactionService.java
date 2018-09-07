2b4024a72605159b8b6a0180afb6f87c778b3eee
Adam Feher <fhradm@gmail.com>
2018-09-07 16:16:30 +0200

    Implemented transfer in TransactionService

--- src/main/java/com/adamfeher/superchargebankingapp/service/TransactionService.java
+++ src/main/java/com/adamfeher/superchargebankingapp/service/TransactionService.java
@@ -3,10 +3,17 @@ package com.adamfeher.superchargebankingapp.service;
 import com.adamfeher.superchargebankingapp.model.Account;
 import com.adamfeher.superchargebankingapp.model.Transaction;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
-public class TransactionService implements BasicTransactionService{
+public class TransactionService implements BasicTransactionService {
+    private AccountService accountService;
+
+    public TransactionService(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
     @Override
     public List<Transaction> getAllTransactions(Account account) {
         return null;
@@ -20,5 +27,22 @@ public class TransactionService implements BasicTransactionService{
     @Override
     public List<Transaction> getTransactionsByType(Account account, Transaction.TransactionDirection type) {
         return null;
+    }
+
+    @Override
+    public void transfer(Account senderAccount, Account receiverAccount, BigDecimal amount) {
+        if (!checkIfValidTransfer(senderAccount,receiverAccount,amount)) {
+            throw new IllegalArgumentException("Amount is wrong or exceed sender limits");
+        } else {
+            senderAccount.setBalance(senderAccount.getBalance().subtract(amount));
+            receiverAccount.setBalance(receiverAccount.getBalance().add(amount));
+            senderAccount.getTransactions().add(new Transaction(amount,senderAccount.getBalance(),Transaction.TransactionDirection.WITHDRAWAL));
+            receiverAccount.getTransactions().add(new Transaction(amount,receiverAccount.getBalance(),Transaction.TransactionDirection.DEPOSIT));
+        }
+
+    }
+
+    private boolean checkIfValidTransfer(Account sender, Account receiver, BigDecimal amount) {
+        return accountService.checkAmountIsNotLessThanZero(amount) && accountService.checkBalanceNotExceedZero(sender, amount);
     }
 }

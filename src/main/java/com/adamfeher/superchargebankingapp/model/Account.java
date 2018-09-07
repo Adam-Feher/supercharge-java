150e2b6bd7a2c2715bfab395521f306fc8fac30e
Adam Feher <fhradm@gmail.com>
2018-09-07 15:10:21 +0200

    Added Account fields and methods

--- src/main/java/com/adamfeher/superchargebankingapp/model/Account.java
+++ src/main/java/com/adamfeher/superchargebankingapp/model/Account.java
@@ -1,4 +1,41 @@
 package com.adamfeher.superchargebankingapp.model;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
+    private User owner;
+    private BigDecimal balance;
+    private List<Transaction> transactions;
+
+    public Account(User owner) {
+        this.owner = owner;
+        this.balance = BigDecimal.ZERO;
+        this.transactions = new ArrayList<>();
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
 }

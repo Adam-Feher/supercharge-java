e1b58de4f6eb5f27d496843e33746fa6709a15a9
Adam Feher <fhradm@gmail.com>
2018-09-07 15:04:30 +0200

    Added User fields and methods

--- src/main/java/com/adamfeher/superchargebankingapp/model/User.java
+++ src/main/java/com/adamfeher/superchargebankingapp/model/User.java
@@ -1,4 +1,45 @@
 package com.adamfeher.superchargebankingapp.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private final String name;
+    private String email;
+    private String phoneNumber;
+    private List<Account> accounts = new ArrayList<Account>();
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public User(String name, String email, String phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
